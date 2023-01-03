8cb3becd63a2ae14ec0db6b0a8c860040a8685d9
Vasilii Tolstoi <eternalwanderer13@yahoo.com>
2023-01-04 02:24:24 +0600

    created class User

--- ordering.py
+++ ordering.py
@@ -2,3 +2,8 @@ class Product:
     def __init__(self, name, price):
         self.name = name
         self.price = price
+
+class User:
+    def __init__(self, login, balance=0):
+        self.login = login
+        self.balance = balance
