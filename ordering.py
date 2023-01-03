2378aae16188c9121848bd885ba0e3d18587b55d
Vasilii Tolstoi <eternalwanderer13@yahoo.com>
2023-01-04 02:25:36 +0600

    added __str__

--- ordering.py
+++ ordering.py
@@ -7,3 +7,6 @@ class User:
     def __init__(self, login, balance=0):
         self.login = login
         self.balance = balance
+
+    def __str__(self):
+        return f'Пользователь {self.login}, баланс - {self.balance}'
